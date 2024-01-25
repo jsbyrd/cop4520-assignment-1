566756ff0d14f009323c5657828073c9197dfac9
Joshua <jsbyrd02@gmail.com>
2024-01-25 12:33:38 -0500

    Add a few comments and rename a few variables

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -6,30 +6,26 @@ import java.util.Arrays;
 public class PrimeNumbers {
   private static final int LIMIT = 100000000;
   public static final int NUM_THREADS = 8;
-  public static final String FILENAME = "summary.txt";
+  public static final String FILENAME = "primes.txt";
 
   public static void main(String[] args) throws InterruptedException, IOException {
     // Create shared counter/sieve among threads
     Sieve sieve = new Sieve(1, LIMIT);
 
     // Create and start threads
-    Thread[] threads = new PrimeThread2[NUM_THREADS];
+    Thread[] threads = new PrimeThread[NUM_THREADS];
     final long executionStartTime = System.currentTimeMillis();
     for (int i = 0; i < NUM_THREADS; i++) {
-      threads[i] = new PrimeThread2(sieve, LIMIT);
+      threads[i] = new PrimeThread(sieve, LIMIT);
       threads[i].start();
     }
     // Wait for threads to die
     for (int i = 0; i < NUM_THREADS; i++) {
       threads[i].join();
     }
-    final long executionEndTime = System.currentTimeMillis();
 
-    // Print execution time
-    System.out.println("Total execution time: " + (executionEndTime - executionStartTime) + "ms");
-
+    // Find sum of primes and number of primes
     boolean[] sieveArray = sieve.getSieveArray();
-
     long sumOfPrimes = 0;
     long numPrimesFound = 0;
     for (int i = 0; i < sieveArray.length; i++) {
@@ -38,7 +34,7 @@ public class PrimeNumbers {
         numPrimesFound++;
       }
     }
-
+    // Find 10 biggest primes
     long[] maxPrimes = new long[10];
     int index = maxPrimes.length - 1;
     for (int i = sieveArray.length - 1; i > 0; i--) {
@@ -48,7 +44,9 @@ public class PrimeNumbers {
         if (index < 0) break;
       }
     }
+    final long executionEndTime = System.currentTimeMillis();
 
+    // Write to primes.txt
     File summary = new File(FILENAME);
     summary.createNewFile();
     FileWriter writer = new FileWriter(FILENAME);
@@ -61,22 +59,21 @@ public class PrimeNumbers {
       writer.write(String.valueOf((i + 1) + ") " + maxPrimes[i]) + "\n");
     }
     writer.close();
-
-
   }
 }
 
-class PrimeThread2 extends Thread {
+class PrimeThread extends Thread {
   private long limit;
   private Sieve sieve;
 
-  public PrimeThread2(Sieve sieve, int limit) {
+  public PrimeThread(Sieve sieve, int limit) {
     this.sieve = sieve;
     this.limit = limit;
   }
 
   @Override
   public void run() {
+    // Sieve of Eratosthenes algorithm
     while (sieve.getValue() <= Math.sqrt(limit)) {
       long currentVal = sieve.getAndIncrement();
       if (sieve.getSieveValue((int) currentVal)) {
@@ -88,14 +85,13 @@ class PrimeThread2 extends Thread {
   }
 }
 
+// Shared counter/array
 class Sieve {
   private long value;
-  private long total;
   private boolean[] sieve;
 
   public Sieve(long value, long limit) {
     this.value = value;
-    this.total = 0;
     this.sieve = new boolean[(int) limit + 1];
     Arrays.fill(sieve, true);
     if (sieve.length > 2) sieve[1] = false;
@@ -106,19 +102,15 @@ class Sieve {
     value++;
     return value;
   }
-
   public synchronized long getValue() {
     return this.value;
   }
-
   public void setSieveToFalse(int index) {
     sieve[index] = false;
   }
-
   public boolean getSieveValue(int index) {
     return sieve[index];
   }
-
   public boolean[] getSieveArray() {
     return sieve;
   }
